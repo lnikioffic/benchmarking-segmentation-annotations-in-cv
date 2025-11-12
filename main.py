0238f84426d34110e978f9805cc41b9d08a28556
lniki <nik_ivanov_33@mail.ru>
2025-11-12 16:25:33 +0500

    test index object

--- main.py
+++ main.py
@@ -6,11 +6,25 @@ from images_eval import (
     compute_contour_accuracy,
     compute_overall_contour_accuracy,
 )
+from PIL import Image
 
 
 def main():
-    gt_mask = cv2.imread('images/cats_rac_test.png')
-    pred_mask = cv2.imread('images/cats_rac_sam.png')
+    mask = np.array(Image.open('images/00000.png'))
+    print(np.unique(mask))
+    gt_mask = np.array(cv2.imread('images/00000.png'))
+    print(np.unique(gt_mask))
+    
+    img = cv2.imread('images/cats_rac_test.png')  # BGR
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    colors, inverse = np.unique(img_rgb.reshape(-1, 3), axis=0, return_inverse=True)
+    mask_indices = inverse.reshape(img_rgb.shape[:2])
+
+    print("Классы:", np.unique(mask_indices))
+    
+    gt_mask = np.array(cv2.imread('images/cats_rac_test.png'))
+    pred_mask = np.array(cv2.imread('images/cats_rac_sam.png'))
 
     print("Уникальные значения в test:", np.unique(gt_mask))
     print("Уникальные значения в sam:", np.unique(pred_mask))
