5b00abb86b9b30f8dd4d3056c5986683d8879430
lniki <nik_ivanov_33@mail.ru>
2025-11-30 21:28:37 +0500

    проход по давис

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+import os
+
 import cv2
 import numpy as np
 from PIL import Image
 
-from contour_detector import get_filtered_bboxes, get_filtered_bboxes_xywh
 from images_eval import (
     compute_boundary_f1,
     compute_contour_accuracy,
     compute_miou,
     compute_overall_contour_accuracy,
 )
+from segmenter import Segmenter
+from utils.contour_detector import (
+    get_filtered_bboxes,
+    get_filtered_bboxes_xywh,
+    getting_coordinates,
+)
+from utils.converter import extract_color_regions, merge_masks
+from utils.mask_display import (  # потестить mask_map на масках с несколькими объектами и посмотреть порядок потом как сам их показывает должен быть одинаковый и тогда можно смореть с несколькими объектами
+    mask_map,
+    visualize_unique_mask,
+)
+from utils.overlay import painter_borders
+from XMem2.inference.interact.interactive_utils import overlay_davis
+
+BASE_PATH = "DAVIS"
+IMAGES_PATH = os.path.join(BASE_PATH, "JPEGImages", "480p")
+MASKS_PATH = os.path.join(BASE_PATH, "Annotations", "480p")
+
+
+# Таблиаца для оценки с начало идёт оценка входной маски, потом идёт оценка по трекеру
 
 
 def mask_segmentation(img) -> np.ndarray:
@@ -21,17 +42,86 @@ def mask_segmentation(img) -> np.ndarray:
     return mask_indices
 
 
+def read_dataset(select_name=None):
+    if select_name is not None:
+        images_folder = filter(lambda x: select_name in x, os.listdir(IMAGES_PATH))
+        mask_folder = filter(lambda x: select_name in x, os.listdir(MASKS_PATH))
+    else:
+        images_folder = os.listdir(IMAGES_PATH)
+        mask_folder = os.listdir(MASKS_PATH)
+
+    data = []
+    for images_path, mask_path in zip(images_folder, mask_folder):
+        images_path = os.path.join(IMAGES_PATH, images_path)
+        mask_path = os.path.join(MASKS_PATH, mask_path)
+        segment = []
+        for image, mask in zip(os.listdir(images_path), os.listdir(mask_path)):
+            image = os.path.join(images_path, image)
+            mask = os.path.join(mask_path, mask)
+            segment.append((image, mask))
+        data.append(segment)
+
+    return data
+
+
 def main():
+    dataest = read_dataset("bear")
     # mask = np.array(Image.open("images/00000.png"))
     # print(np.unique(mask))
     # gt_mask = np.array(cv2.imread("images/00000.png"))
     # print(np.unique(gt_mask))
+    i = np.array(cv2.imread("00000.png"))
+    im = mask_segmentation(i)
+    gray = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+    print(get_filtered_bboxes_xywh(gray, min_area_ratio=0.001))
+    print(len(mask_map(im)))
+    im = painter_borders(i, gray)
+    cv2.imshow("imgg", im)
 
-    img = np.array(cv2.imread("images/00000.png"))  # BGR
-    mask_segmentation(img)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_mask = np.array(cv2.imread("images/00000.png"))  # BGR
+    mask_segmentation(img_mask)
+    gray = cv2.cvtColor(img_mask, cv2.COLOR_BGR2GRAY)
     print(get_filtered_bboxes_xywh(gray, min_area_ratio=0.001))
     print(get_filtered_bboxes(gray, min_area_ratio=0.001))
+    box = get_filtered_bboxes(gray, min_area_ratio=0.001)
+
+    image = np.array(cv2.imread("images/00000.jpg"))
+    im = painter_borders(image, gray)
+    cv2.imshow("img", im)
+    segmenter = Segmenter()
+    segmenter.set_image(image)
+
+    prompt = {
+        "boxes": np.array(box),
+    }
+    maskss = []
+    masks, scores, logits = segmenter.predict(prompt, mode="box")
+    maskss.append(masks[np.argmax(scores)])
+
+    mask, unique_mask = merge_masks(maskss)
+
+    mask_indices, colors = extract_color_regions(unique_mask)
+    print("Классы:", np.unique(mask_indices))
+
+    f = overlay_davis(image, mask_indices)
+    f = cv2.cvtColor(f, cv2.COLOR_BGR2RGB)
+    mask = visualize_unique_mask(mask_indices)
+    cv2.imshow("mask", mask)
+    cv2.imshow("overlay", f)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    mask = np.array(cv2.imread("images/cats_rac_test.png"))
+    gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    # Последним идёт самая задняя кошка
+    for m in mask_map(gray):
+        # print(get_filtered_bboxes_xywh(m, min_area_ratio=0.001))
+        # print(get_filtered_bboxes(m, min_area_ratio=0.001))
+        print(getting_coordinates(m))
+    im = painter_borders(mask, gray)
+    cv2.imshow("img", im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     gt_mask = mask_segmentation(np.array(cv2.imread("images/cats_rac_test.png")))
     pred_mask = mask_segmentation(np.array(cv2.imread("images/cats_rac_sam.png")))
---
+++ tracker_core_xmem2.py
@@ -0,0 +1,140 @@
+import cv2
+import numpy as np
+import psutil
+import torch
+from XMem2.inference.inference_core import InferenceCore
+from XMem2.model.network import XMem
+from XMem2.inference.data.mask_mapper import MaskMapper
+from config import XMEM_CONFIG, DEVICE
+from torchvision import transforms
+from XMem2.util.range_transform import im_normalization
+from XMem2.inference.interact.interactive_utils import overlay_davis
+from segmenter import Segmenter
+from utils.mask_display import visualize_wb_mask, mask_map
+from utils.contour_detector import getting_coordinates
+from utils.converter import merge_masks, extract_color_regions
+
+
+class TrackerCore:
+
+    name_version = 'XMem2'
+
+    def __init__(self, device: str = DEVICE):
+        self.device = device
+        if self.device.lower() != 'cpu':
+            self.network = XMem(XMEM_CONFIG, 'checkpoints/XMem.pth').eval().to('cuda')
+        else:
+            self.network = XMem(
+                XMEM_CONFIG, 'checkpoints/XMem.pth', map_location='cpu'
+            ).eval()
+        self.processor = InferenceCore(self.network, XMEM_CONFIG)
+
+        self.im_transform = transforms.Compose(
+            [transforms.ToTensor(), im_normalization]
+        )
+        self.mapper = MaskMapper()
+
+    @torch.no_grad()
+    def track(
+        self, frame: np.ndarray, mask_segmet: np.ndarray | None = None, exhaustive=False
+    ):
+        if mask_segmet is not None:
+            mask, labels = self.mapper.convert_mask(mask_segmet, exhaustive)
+            mask = torch.Tensor(mask).to(self.device)
+            self.processor.set_all_labels(list(self.mapper.remappings.values()))
+        else:
+            mask = None
+            labels = None
+
+        frame_tensor = self.im_transform(frame).to(self.device)
+        probs = self.processor.step(frame_tensor, mask, labels)
+
+        out_mask = torch.argmax(probs, dim=0)
+        out_mask = (out_mask.detach().cpu().numpy()).astype(np.uint8)
+        final_mask = np.zeros_like(out_mask)
+
+        # map back
+        for k, v in self.mapper.remappings.items():
+            final_mask[out_mask == v] = k
+
+        return final_mask
+
+    @torch.no_grad()
+    def clear_memory(self):
+        self.processor.clear_memory()
+        self.mapper.clear_lables()
+        torch.cuda.empty_cache()
+
+
+if __name__ == '__main__':
+    path = 'video-test/video.mp4'
+    video = cv2.VideoCapture(path)
+    ret, frame = video.read()
+    frame_cop = frame.copy()
+    video.release()
+
+    bboxes = [(476, 166, 102, 154), (8, 252, 91, 149), (106, 335, 211, 90)]
+    points = [[531, 230], [45, 321], [226, 360], [194, 313]]
+
+    mode = 'point'
+    prompts = {
+        'point_coords': np.array([[531, 230], [45, 321], [226, 360], [194, 313]]),
+        'point_labels': np.array([1] * len(points)),
+    }
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    seg = Segmenter()
+    seg.set_image(frame)
+
+    maskss = []
+    for point in points:
+        prompts = {
+            'point_coords': np.array([point]),
+            'point_labels': np.array([1]),
+        }
+        masks, scores, logits = seg.predict(prompts, mode)
+        maskss.append(masks[np.argmax(scores)])
+    _, unique_mask = merge_masks(maskss)
+    mask_indices, colors = extract_color_regions(unique_mask)
+    print('Классы:', np.unique(mask_indices))
+
+    masks = []
+    images = []
+    traker = TrackerCore()
+    frames_to_propagate = 200
+    current_frame_index = 0
+    cap = cv2.VideoCapture(path)
+    while cap.isOpened():
+        current_memory_usage = psutil.virtual_memory().percent
+        # print(current_memory_usage)
+        if current_memory_usage > 90:
+            break
+        ret, frame_v = cap.read()
+        if not ret:
+            break
+        if current_frame_index > frames_to_propagate:
+            break
+
+        if current_frame_index == 0:
+            mask = traker.track(frame_v, mask_indices)
+            masks.append(mask)
+            images.append(frame_v)
+        else:
+            mask = traker.track(frame_v)
+            masks.append(mask)
+            images.append(frame_v)
+
+        current_frame_index += 1
+    video.release()
+    im3 = visualize_wb_mask(masks[200])
+    ima = images[200].copy()
+    for m in mask_map(masks[200]):
+        for box in getting_coordinates(m):
+            (x, y, w, h) = [v for v in box]
+            cv2.rectangle(ima, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    image_m = overlay_davis(images[200], masks[200])
+    cv2.imshow('image_m200', image_m)
+    cv2.imshow('ima_rect', ima)
+    cv2.imshow('im2', im3)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
